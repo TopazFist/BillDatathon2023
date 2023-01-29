bd7d31bb4f6550ff5ce0422f436ac519a3474c68
irrundle3 <irrundle3@github.com>
2023-01-29 01:24:46 -0600

    lavenshtein in c

--- modules/lavenshtein.py
+++ modules/lavenshtein.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import os
+from fuzzywuzzy import fuzz
+from Levenshtein import distance
 
 """
 Calculates the lavenshtein distance between two strings.
@@ -13,37 +15,43 @@ deletions, and edits differently
 Credit to GeeksForGeeks.org for this algorithm that I modified
 """
 
-def lavenshtein(str1, str2, p_insert=1, p_delete=1, p_edit=1):
-    # Create a table to store results of subproblems
-    dp = np.zeros((len(str1) + 1, len(str2) + 1))
+def partial_lavenshtein(str1, str2):
+    return fuzz.partial_ratio(str1, str2)
+
+def lavenshtein(str1, str2):
+    return distance(str1, str2)
+
+# def lavenshtein(str1, str2, p_insert=1, p_delete=1, p_edit=1):
+#     # Create a table to store results of subproblems
+#     dp = np.zeros((len(str1) + 1, len(str2) + 1))
  
-    # Fill d[][] in bottom up manner
-    for i in range(len(str1) + 1):
-        for j in range(len(str2) + 1):
+#     # Fill d[][] in bottom up manner
+#     for i in range(len(str1) + 1):
+#         for j in range(len(str2) + 1):
  
-            # If first string is empty, only option is to
-            # insert all characters of second string
-            if i == 0:
-                dp[i,j] = j    # Min. operations = j
+#             # If first string is empty, only option is to
+#             # insert all characters of second string
+#             if i == 0:
+#                 dp[i,j] = j    # Min. operations = j
  
-            # If second string is empty, only option is to
-            # remove all characters of second string
-            elif j == 0:
-                dp[i,j] = i    # Min. operations = i
+#             # If second string is empty, only option is to
+#             # remove all characters of second string
+#             elif j == 0:
+#                 dp[i,j] = i    # Min. operations = i
  
-            # If last characters are same, ignore last char
-            # and recur for remaining string
-            elif str1[i-1] == str2[j-1]:
-                dp[i,j] = dp[i-1,j-1]
+#             # If last characters are same, ignore last char
+#             # and recur for remaining string
+#             elif str1[i-1] == str2[j-1]:
+#                 dp[i,j] = dp[i-1,j-1]
  
-            # If last character are different, consider all
-            # possibilities and find minimum
-            else:
-                dp[i][j] = min(dp[i,j-1] + p_insert,        # Insert
-                                   dp[i-1,j] + p_delete,        # Remove
-                                   dp[i-1,j-1] + p_edit)    # Replace
+#             # If last character are different, consider all
+#             # possibilities and find minimum
+#             else:
+#                 dp[i][j] = min(dp[i,j-1] + p_insert,        # Insert
+#                                    dp[i-1,j] + p_delete,        # Remove
+#                                    dp[i-1,j-1] + p_edit)    # Replace
  
-    return int(dp[-1,-1])
+#     return int(dp[-1,-1])
 
 def get_laven(value):
     columns = ["BB1", "BB2", "BB3", "BB4", "BB5", "BB6", "BB7", "BB8", "Text_Main"]
@@ -77,10 +85,9 @@ def get_laven(value):
         text_col = ocr_temp["Text_Main"]
         levan_name = text_col.apply(lavenshtein, str2=value["vendor_name"]).min()
         levan_price = text_col.apply(lavenshtein, str2=str(value["amount"])).min()
-        levan_date = text_col.apply(lavenshtein, str2=str(value["date"])).min()
-        levan_address = text_col.apply(lavenshtein, str2=value["vendor_address"][:30]).min()
+        levan_date = text_col.apply(partial_lavenshtein, str2=str(value["date"])).max()
+        levan_address = text_col.apply(lavenshtein, str2=value["vendor_address"][:40]).min()
         levan_user.loc[levan_user.shape[0]]=[filename[:-4],levan_name,levan_price,levan_date,levan_address]
-
     pot = levan_user[(levan_user['levan_name'] < 5) | (levan_user['levan_address'] < 10)]
     pot_doc = pot['doc_id'].values.tolist()
     pot_name = pot['levan_name'].values.tolist()
